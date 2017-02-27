43c0970f6f4231faa6245a9ee1b47f986e72e3ee
Erebus570 <JMathews@student.neumont.edu>
2017-02-27 14:48:14 -0700

    I have most if not all the methods set up for file editor (no logic)

--- PFTCSM/src/app/FileEditor.java
+++ PFTCSM/src/app/FileEditor.java
@@ -1,10 +1,272 @@
 package app;
 
+import enums.*;
+import enums.Class;
 import models.*;
 
 public class FileEditor {
 
 	public static boolean sanitize(CharacterSheet sheet) {
+		
+	}
+	
+	public static String takeInName() {
+		
+	}
+	
+	public static Alignment takeInAlignment() {
+		
+	}
+	
+	public static String takeInPlayerName() {
+		
+	}
+	
+	public static Class takeInClass() {
+		
+	}
+	
+	public static int takeInLevel() {
+		
+	}
+	
+	public static Deity takeInDeity() {
+		
+	}
+	
+	public static String takeInHomeland() {
+		
+	}
+	
+	public static Race takeInRace() {
+		
+	}
+	
+	public static String takeInSize() {
+		
+	}
+	
+	public static Gender takeInGender() {
+		
+	}
+	
+	public static int takeInAge() {
+		
+	}
+	
+	public static String takeInHeight() {
+		
+	}
+	
+	public static int takeInWeight() {
+		
+	}
+	
+	public static String takeInHair() {
+		
+	}
+	
+	public static String takeInEyes() {
+		
+	}
+	
+	public static void takeInAbilityBase(Ability ability) {
+		
+	}
+	
+	public static void takeInAbilityTemp(Ability ability) {
+		
+	}
+	
+	public static void determineAbilityMod(Ability ability) {
+		
+	}
+	
+	public static void determineAblityTempMod(Ability ability) {
+		
+	}
+	
+	public static int takeInBaseHP() {
+		
+	}
+	
+	public static int takeInCurHP() {
+		
+	}
+	
+	public static int takeInBaseSpeed() {
+		
+	}
+	
+	public static int takeInSpeedWithArmor() {
+		
+	}
+	
+	public static int takeInFlySpeed() {
+		
+	}
+	
+	public static int takeInSwimSpeed() {
+		
+	}
+	
+	public static int takeInClimbSpeed() {
+		
+	}
+	
+	public static int takeInBurrowSpeed() {
+		
+	}
+	
+	public static int takeInSpeedMod() {
+		
+	}
+	
+	public static int takeInNonLethalDamage() {
+		
+	}
+	
+	public static void addWeapon() {
+		
+	}
+	
+	public static void addArmor() {
+		
+	}
+	
+	public static void addItem() {
+		
+	}
+	
+	public static void addSpell() {
+		
+	}
+	
+	public static int takeInArmorBonus() {
+		
+	}
+	
+	public static int takeInSheildBonus() {
+		
+	}
+	
+	public static int takeInSizeArmorMod() {
+		
+	}
+	
+	public static int takeInNaturalArmor() {
+		
+	}
+	
+	public static int takeInDeflectArmorMod() {
+		
+	}
+	
+	public static int takeInMiscArmorMod() {
+		
+	}
+	
+	public static int takeInBaseSaveF() {
+		
+	}
+	
+	public static int takeInBaseSaveR() {
+		
+	}
+	
+	public static int takeInBaseSaveW() {
+		
+	}
+	
+	public static int takeInMagicModF() {
+		
+	}
+	
+	public static int takeInMagicModR() {
+		
+	}
+	
+	public static int takeInMagicModW() {
+		
+	}
+	
+	public static int takeInMiscModF() {
+		
+	}
+	
+	public static int takeInMiscModR() {
+		
+	}
+	
+	public static int takeInMiscModW() {
+		
+	}
+	
+	public static int takeInTempModF() {
+		
+	}
+	
+	public static int takeInTempModR() {
+		
+	}
+	
+	public static int takeInTempModW() {
+		
+	}
+	
+	public static int takeInBaseAttackBonus() {
+		
+	}
+	
+	public static int takeInExperience() {
+		
+	}
+	
+	public static int determineExpToNextLvl() {
+		
+	}
+	
+	public static int takeInCP() {
+		
+	}
+	
+	public static int takeInSP() {
+		
+	}
+	
+	public static int takeInGP() {
+		
+	}
+	
+	public static int takeInPP() {
+		
+	}
+	
+	public static String takeInSpellResistance() {
+		
+	}
+	
+	public static void addSkill() {
+		
+	}
+	
+	public static void addAbility() {
+		
+	}
+	
+	public static int touchAC() {
+		
+	}
+	
+	public static int flatFootedAc() {
+		
+	}
+	
+	public static int determineCMB() {
+		
+	}
+	
+	public static int determineCMD() {
 		
 	}
 }
