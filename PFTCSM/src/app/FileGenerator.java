09d67abf1c3bb832d020da1b450c598b4e62d2ed
Erebus570 <JMathews@student.neumont.edu>
2017-03-07 14:46:03 -0700

    Created FileGenerator class

---
+++ PFTCSM/src/app/FileGenerator.java
@@ -0,0 +1,22 @@
+package app;
+
+import java.util.HashMap;
+
+import models.*;
+import enums.*;
+import enums.Class;
+
+public class FileGenerator {
+
+	public static int randomAbilityScore() {
+		return 0;
+	}
+	
+	public static HashMap<NameOfSkill, Skill> randomizeSkills(Class chaClass) {
+		return null;
+	}
+	
+	public static CharacterSheet startRandomCharacter(String name, Class chaClass, Race race, int level) {
+		return null;
+	}
+}
